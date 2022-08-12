965925fb0b46578677f7b1989e4071686c95882f
leishuai9510 <leishuai.9510@bytedance.com>
2022-08-12 17:00:44 +0800

    增加部署命令

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+scp ./target/TestGit.war \
+leishuai.9510@10.37.156.152:/home/leishuai.9510/apache-tomcat-8.5.81/webapps/majiang.war || \
+scp ./target/majiang.war \
+leishuai.9510@10.37.156.152:/home/leishuai.9510/apache-tomcat-8.5.81/webapps/majiang.war \
